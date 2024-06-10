117690d9d521b9a2a053ad56cd15a622abd9eebe
katas8n1 <kirka6061512@icloud.com>
2024-06-10 19:01:10 +0200

    The second Java  lesson

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+// Initial point of a program
+class Main {
+    // public
+    // static
+    // void
+    // main
+
+    public static void main(String [] people) {
+//        for(int i = 0; i < people.length; i++) {
+//            System.out.println("Welcome to the gey-party : " + people[i]);
+//
+//        }
+//
+//        short widthOfLine = 31000;
+//        byte widthOfSquare = -120;
+//        int widthOfCircle = 2138437023;
+
+//        System.out.println(widthOfLine);
+//        System.out.println(widthOfSquare);
+//        System.out.println(widthOfCircle);
+ // Hello
+        char correctAnswer = '@';
+        boolean isDrunk = true;
+
+
+        // Comment
+        System.out.println(correctAnswer);
+    }
+}
